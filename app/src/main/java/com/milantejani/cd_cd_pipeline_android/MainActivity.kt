0c6253415a7fc372b96893dbc4a84b5b19083372
Milan Tejani <milantejani7435@gmail.com>
2025-02-28 14:48:18 +0530

    added some UI and removed lint report html

--- app/src/main/java/com/milantejani/cd_cd_pipeline_android/MainActivity.kt
+++ app/src/main/java/com/milantejani/cd_cd_pipeline_android/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.milantejani.cd_cd_pipeline_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val appVersion = "${BuildConfig.VERSION_CODE} || ${BuildConfig.VERSION_NAME}"
+        binding.txtAppVersion.text=String.format(getString(R.string.app_version), appVersion)
     }
 }
